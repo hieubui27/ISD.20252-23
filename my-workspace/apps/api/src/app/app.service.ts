eead8cd6b4dc366717f8024d1226982dc99ea714
Tu <ntuantua5@gmail.com>
2026-04-26 17:14:59 +0700

    feat: setup prisma orm

--- my-workspace/apps/api/src/app/app.service.ts
+++ my-workspace/apps/api/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@my-workspace/datatypes';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  getData(): User {
+    return { 
+      id: '1',
+      name: 'John Doe',
+      email: 'abc@gmail.com'
+     };
   }
 }
---
+++ my-workspace/packages/shared/datatypes/README.md
@@ -0,0 +1,3 @@
+# datatypes
+
+This library was generated with [Nx](https://nx.dev).
