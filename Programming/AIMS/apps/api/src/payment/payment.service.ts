c3b1b6952b373029a68fbaf176137c8968a6b2e7
Tu <ntuantua5@gmail.com>
2026-05-27 11:02:54 +0700

    fix: add convert money to USD function

--- Programming/AIMS/apps/api/src/payment/payment.service.ts
+++ Programming/AIMS/apps/api/src/payment/payment.service.ts
@@ -22,6 +22,7 @@ import {
   PlaceOrderPaymentPort,
 } from './ports/place-order-payment.port';
 import {
+  convertMoneyToUSD,
   ensureCanMarkFailed,
   ensureCanMarkRefundRequired,
   ensureCanMarkSuccess,
@@ -118,7 +119,9 @@ export class PaymentService {
       };
     }
 
-    const paypalOrder = await this.paypalService.createOrder(dto.amount);
+    const paypalOrder = await this.paypalService.createOrder(
+      convertMoneyToUSD(dto.amount),
+    );
     const approvalLinks = (paypalOrder?.links || []) as PaypalApprovalLink[];
     const approveLink =
       approvalLinks.find((link) => link.rel === 'approve')?.href || '';
