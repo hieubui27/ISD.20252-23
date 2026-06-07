22c3dad48cb9d791d5a5eb543b43ce4c85bcc5f0
Tu <ntuantua5@gmail.com>
2026-06-08 00:03:49 +0700

    refactor: move payment folder into app folder

--- Programming/AIMS/apps/client/src/app/payment/payment.ts
+++ Programming/AIMS/apps/client/src/app/payment/payment.ts
@@ -1,11 +1,11 @@
 // src/app/payment/payment.ts
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { PaymentPageFacade } from '../app/payment/facades/payment-page.facade';
-import { AimsFooterComponent } from '../app/shared/layout/aims-footer/aims-footer';
-import { AimsHeaderComponent } from '../app/shared/layout/aims-header/aims-header';
-import { AimsButtonComponent } from '../app/shared/ui/aims-button/aims-button';
-import { StatusMessageComponent } from '../app/shared/ui/status-message/status-message';
+import { PaymentPageFacade } from './facades/payment-page.facade';
+import { AimsFooterComponent } from '../shared/layout/aims-footer/aims-footer';
+import { AimsHeaderComponent } from '../shared/layout/aims-header/aims-header';
+import { AimsButtonComponent } from '../shared/ui/aims-button/aims-button';
+import { StatusMessageComponent } from '../shared/ui/status-message/status-message';
 
 @Component({
   selector: 'app-payment',
